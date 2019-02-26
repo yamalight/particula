b16c5ccff0c4c3891eb2848825b75bcb9ea80dce
Igor Samokhovets <igor@samohovets.ru>
2019-02-26 21:54:18 +0300

    Add linebreak, double quotes to single quotes

--- bin/particula.js
+++ bin/particula.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-const yargs = require("yargs");
+const yargs = require('yargs');
 
-const { start } = require("../index.js");
+const { start } = require('../index.js');
 
-const package = require("../package.json");
+const package = require('../package.json');
 
 // CLI config
 
@@ -11,20 +11,21 @@ yargs
   .version(package.version)
   .help()
   .command({
-    command: "*",
-    describe: "start server",
+    command: '*',
+    describe: 'start server',
     builder: {
       port: {
-        alias: "p",
-        description: "Start server at specified port",
-        type: "number"
+        alias: 'p',
+        description: 'Start server at specified port',
+        type: 'number'
       }
     },
     handler({ port }) {
       if (isNaN(port)) {
-        console.error("Invalid port");
+        console.error('Invalid port');
         process.exit(1);
       }
       start(port);
     }
   }).argv;
+
