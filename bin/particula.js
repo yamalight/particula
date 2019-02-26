2139bfe38546c7a1bb9548df2ee865cb599ee892
Igor Samokhovets <igor@samohovets.ru>
2019-02-26 21:00:58 +0300

    Add yargs, CLI help section, port selection

--- bin/particula.js
+++ bin/particula.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
-const {start} = require('../index.js');
-start();
+const yargs = require('yargs');
+
+const start = require('../index.js');
+
+const package = require('../package.json');
+
+yargs
+    .version(package.version)
+    .help()
+    .demand(1)
+    .command(start)
+    .argv
