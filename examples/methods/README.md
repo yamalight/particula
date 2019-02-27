e7a0292b7fe6187f19e766027810dec5f9378882
Tim Ermilov <yamalight@gmail.com>
2019-02-27 15:06:11 +0100

    Add HTTP methods example
    Addresses #9

---
+++ examples/methods/README.md
@@ -0,0 +1,7 @@
+# HTTP methods Particula app example
+
+## How to use
+
+1. Start the app by running `npm start`
+2. Navigate to `http://localhost:8080` to see GET response
+3. Send POST / PUT / DELETE request to `http://localhost:8080` to see corresponding response
---
+++ examples/methods/routes/index.js
@@ -0,0 +1,7 @@
+exports.get = (req, res) => res.send('This will be returned on GET request');
+
+exports.post = (req, res) => res.send('This will be returned on POST request');
+
+exports.put = (req, res) => res.send('This will be returned on PUT request');
+
+exports.delete = (req, res) => res.send('This will be returned on DELETE request');
