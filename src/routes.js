bf2f99781f8f74db2bc9c071d4cf7240f733c6a4
Tim Ermilov <yamalight@gmail.com>
2019-02-27 13:20:30 +0100

    Add basic hot-reloading for routes Closes #3

--- src/routes.js
+++ src/routes.js
@@ -1,34 +1,45 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const chokidar = require('chokidar');
 const {routesPath} = require('./config');
 
-// wraps user router files into a new router instance
-const wrapMiddleware = (app, fn) => {
-  const router = express.Router();
-  router.getData = app.get.bind(app);
-  fn(router);
-  return router;
-};
+// stored map of routes and corresponding routers
+// used for hot swapping the modules in development
+const routesMap = {};
 
-const loadFile = (app, fileName) => {
+// dummy router that does nothing
+// user for failed route init and deleted routes
+const dummyRouter = (req, res, next) => next();
+
+// converts file name to route name
+const fileNameToRoute = fileName => {
   const fileBaseName = fileName.replace(/\.js$/, '');
   const routeName = fileBaseName === 'index' ? '/' : `/${fileBaseName.toLowerCase()}`;
+  return routeName;
+};
+
+// loads given file into router
+const loadFile = (app, fileName) => {
+  const routeName = fileNameToRoute(fileName);
   const routeHandler = require(path.join(routesPath, fileName));
 
+  const router = express.Router();
+
   if (typeof routeHandler.default === 'function') {
-    app.get(routeName, routeHandler.default);
-    return;
+    router.get(routeName, routeHandler.default);
+    return {router, routeName};
   }
 
   if (typeof routeHandler === 'function') {
-    app.get(routeName, routeHandler);
-    return;
+    router.get(routeName, routeHandler);
+    return {router, routeName};
   }
 
   if (routeHandler.useRouter && typeof routeHandler.useRouter === 'function') {
-    app.use(wrapMiddleware(app, routeHandler.useRouter));
-    return;
+    router.app = app;
+    routeHandler.useRouter(router);
+    return {router, routeName};
   }
 
   const allowedMethods = ['put', 'get', 'post', 'delete'];
@@ -38,8 +49,68 @@ const loadFile = (app, fileName) => {
   }
 
   methods.forEach(method => {
-    app[method](routeName, routeHandler[method]);
+    router[method](routeName, routeHandler[method]);
   });
+
+  return {router, routeName};
+};
+
+// (re-)registers routes on the app when needed
+const registerRoutes = app => {
+  Object.keys(routesMap).forEach(route => {
+    // skip already registered routes
+    if (routesMap[route].register) {
+      return;
+    }
+    // create new register function, indicates that route has been registered
+    routesMap[route].register = (req, res, next) => routesMap[route].router(req, res, next);
+    // use register function in server
+    app.use(routesMap[route].register);
+  });
+};
+
+// sets up hot reload for routes
+const setupHotReload = app => {
+  chokidar
+    .watch(path.join(routesPath, '*.js'), {ignoreInitial: true})
+    .on('add', fullpath => {
+      try {
+        const {base: fileName} = path.parse(fullpath);
+        const routeName = fileNameToRoute(fileName);
+        routesMap[routeName] = {router: dummyRouter, register: undefined};
+        const {router} = loadFile(app, fileName);
+        routesMap[routeName].router = router;
+        registerRoutes(app);
+        console.log(`File ${fileName} has been added, loaded new route ${routeName}`);
+      } catch (e) {
+        console.error('Error adding new route:', e);
+      }
+    })
+    .on('change', fullpath => {
+      try {
+        const {base: fileName} = path.parse(fullpath);
+        // delete cached required script
+        delete require.cache[fullpath];
+        // load new version
+        const {router, routeName} = loadFile(app, fileName);
+        routesMap[routeName].router = router;
+        registerRoutes(app);
+        console.log(`File ${fileName} has been change, updated existing route ${routeName}`);
+      } catch (e) {
+        console.error('Error updating route:', e);
+      }
+    })
+    .on('unlink', fullpath => {
+      try {
+        const {base: fileName} = path.parse(fullpath);
+        const routeName = fileNameToRoute(fileName);
+        routesMap[routeName].router = dummyRouter;
+        delete require.cache[fullpath];
+        console.log(`File ${fileName} has been removed, removed existing route ${routeName}`);
+      } catch (e) {
+        console.error('Error removing route:', e);
+      }
+    });
 };
 
 // loads and sets up all user routes
@@ -48,10 +119,20 @@ const setupRoutes = app => {
   if (!fs.existsSync(routesPath)) {
     throw new Error(`Routes path doesn't exist! Please create routes/ folder.`);
   }
-  // load routes
+
+  // load routes into memory
   const routesFiles = fs.readdirSync(routesPath);
   for (const fileName of routesFiles) {
-    loadFile(app, fileName);
+    const {router, routeName} = loadFile(app, fileName);
+    routesMap[routeName] = {router, register: undefined};
+  }
+
+  // add routes to express
+  registerRoutes(app);
+
+  // setup hot reload if not running in production
+  if (process.env.NODE_ENV !== 'production') {
+    setupHotReload(app);
   }
 };
 
